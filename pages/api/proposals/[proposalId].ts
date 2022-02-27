e4179779fc601c7e586a393512082be9db3cc891
Joe Hsu <jhsu.x1@gmail.com>
2022-02-27 13:52:23 -0500

    initial nextjs commit

---
+++ pages/api/proposals/[proposalId].ts
@@ -0,0 +1,134 @@
+import { supabase } from "~/store/supabase";
+import { ethers } from "ethers";
+
+import {
+  associationTokenIds,
+  signMessage,
+  useSigner,
+  verifyAssociationMembership,
+} from "~/utils/tokenHolders";
+
+interface Vote {
+  yes: boolean;
+}
+interface Proposal {
+  id: number;
+  title: string;
+  description: string;
+  votes: Vote[];
+}
+
+export async function handler(req, res) {
+  if (req.method === "POST") {
+    return res.json({ message: "hi" });
+  }
+  return res.json({ message: "hi" });
+}
+/*
+import invariant from "tiny-invariant";
+
+interface Vote {
+  yes: boolean;
+}
+interface Proposal {
+  id: number;
+  title: string;
+  description: string;
+  votes: Vote[];
+}
+
+export const action: ActionFunction = async ({request, params}) => {
+  invariant(params.proposalId, "Expected params.proposalId");
+  if (request.method !== "POST") {
+    return json({message: "Method not allowed"}, 405);
+  }
+
+  // TODO: handle what they are voting on
+  const payload = await request.formData();
+  const message = payload.get("message") as string;
+  const signature = payload.get("signature") as string;
+  const yes = payload.get("yes") === "true";
+  const actualAddress = ethers.utils.verifyMessage(message, signature);
+
+  // get association plot ids
+  // TODO: get proposal trait type and value
+  const plotIds = await associationTokenIds({
+    traitType: "Plot Type",
+    value: "Train Station",
+  });
+  // check if the wallet is holding any of the association tokens
+  const isMember = await verifyAssociationMembership(
+    actualAddress,
+    plotIds.map((id) => id.toString())
+  );
+
+  if (isMember) {
+    // create vote
+    await supabase
+      .from("votes")
+      .insert({proposal_id: params.proposalId, address: actualAddress, yes});
+  }
+
+  return new Response(
+    JSON.stringify({
+      address: actualAddress,
+      isMember,
+    }),
+    {
+      status: 200,
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }
+  );
+};
+
+export const loader: LoaderFunction = async ({params}) => {
+  invariant(params.proposalId, "Expected params.proposalId");
+  const {data, error} = await supabase
+    .from<Proposal>("proposals")
+    .select(`id, title, description, votes ( yes ) as yes`)
+    .filter("id", "eq", params.proposalId)
+    .limit(1)
+    .single();
+  if (error) {
+    throw new Error(error.message);
+  }
+  return data;
+};
+
+export default function Propsal() {
+  const data = useLoaderData<Proposal>();
+  const fetcher = useFetcher();
+
+  const signer = useSigner();
+
+  const onSubmit = async (yes: boolean) => {
+    if (!signer) return;
+    const payload = await signMessage(signer);
+    if (payload) {
+      fetcher.submit({...payload, yes: yes.toString()}, {method: "post"});
+    }
+  };
+
+  const voteYes = () => {
+    onSubmit(true);
+  };
+  const voteNo = () => {
+    onSubmit(false);
+  };
+
+  return (
+    <div>
+      <h2>{data.title}</h2>
+      <p>{data.description}</p>
+      <div>
+        yes: {data.votes.filter((v) => v.yes).length} / {data.votes.length}
+      </div>
+      <button onClick={voteYes}>Yes</button>
+      <button onClick={voteNo}>No</button>
+    </div>
+  );
+}
+
+*/
---
+++ utils/tokenHolders.ts
@@ -0,0 +1,100 @@
+import { ethers } from "ethers";
+import { useEffect, useState } from "react";
+import { supabase } from "~/store/supabase";
+
+// Query for token ids held by the adddress
+const OWNER_QUERY = `
+query OwnedTokenss($address: String!, $tokenIds: [String!]) {
+  user(id: $address) {
+    id
+    tokens(where: { id_in: $tokenIds}) {
+      id
+    }
+  }
+}
+`;
+
+// query the subgraph to check if an address is holding any of the tokens
+async function verifyHolder(
+  address: string,
+  tokenIds: string[]
+): Promise<boolean> {
+  const result = await fetch(
+    "https://api.thegraph.com/subgraphs/name/cosmoburn/turf-nft",
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        query: OWNER_QUERY,
+        variables: {
+          address,
+          tokenIds,
+        },
+      }),
+    }
+  );
+  const body = await result.json();
+  return (body.data.user?.tokens.length ?? 0) > 0;
+}
+
+export async function associationTokenIds({
+  traitType,
+  value: traitValue,
+}: {
+  traitType: string;
+  value: string;
+}) {
+  // get token ids for an association
+  const { data, error } = await supabase
+    .from("metadata")
+    .select("id")
+    .contains(
+      "attributes",
+      JSON.stringify([{ value: traitValue, trait_type: traitType }])
+    );
+
+  if (error) {
+    throw new Error(error.message);
+  }
+  return data?.map((row) => row.id) ?? [];
+}
+
+export async function verifyAssociationMembership(
+  address: string,
+  tokenIds: string[]
+) {
+  return verifyHolder(address, tokenIds);
+}
+
+export const signMessage = async (signer: ethers.Signer) => {
+  if (!signer) return null;
+  const address = await signer.getAddress();
+  if (address) {
+    const message = `address ${address}`;
+    const signature = await signer.signMessage(message);
+    const payload = {
+      message,
+      signature,
+    };
+    return payload;
+  }
+  return null;
+};
+
+export const useSigner = () => {
+  const [signer, setSigner] = useState<ethers.Signer | null>(null);
+
+  useEffect(() => {
+    // @ts-ignore
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    (async () => {
+      await provider.send("eth_requestAccounts", []);
+      const signer = provider.getSigner();
+      setSigner(signer);
+    })();
+  }, []);
+
+  return signer;
+};
